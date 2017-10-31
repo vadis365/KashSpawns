487774e5037acd91fb80a52e8023121eff54d79a
vadis365 <curve_linear@hotmail.com>
2017-10-31 18:02:19 +0000

    Changed up stuff for Kash to add to KC

--- src/main/java/kash_spawns/events/EntityAddAttackEvent.java
+++ src/main/java/kash_spawns/events/EntityAddAttackEvent.java
@@ -3,7 +3,6 @@ package kash_spawns.events;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityCreature;
 import net.minecraft.entity.EntityLivingBase;
-import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.EntityAIAttackMelee;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
@@ -25,21 +24,9 @@ public class EntityAddAttackEvent {
 		if (event.getEntity() instanceof EntityCreature && event.getEntity().isNonBoss() && !(event.getEntity() instanceof IMob)) {
 			EntityCreature entity = (EntityCreature) event.getEntity();
 			World world = entity.getEntityWorld();
-			if (!world.isRemote) {
-				
-				// setting a generic damage attribute seems to spam the console - but it does seem to work
-				setUpAttributes(entity);
+			if (!world.isRemote)
 				setUpAI(entity);
-			}
 		}
-	}
-
-	public void setUpAttributes(EntityCreature entity) {
-		boolean setAttribute = true;
-		if(entity.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE) != null)
-			setAttribute = false;
-		if (setAttribute)
-			entity.getAttributeMap().registerAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(2.0D);
 	}
 
 	public void setUpAI(EntityCreature entity) {
@@ -48,16 +35,13 @@ public class EntityAddAttackEvent {
 		 if (ai instanceof EntityAIPanic)
 			 entity.tasks.removeTask(ai);
 		 }
-			entity.tasks.addTask(1, new AIPassiveMobAttack(entity));
-
-			// the boolean sets if other mobs of it's type help it (like pigmen)
-			entity.targetTasks.addTask(1, new EntityAIHurtByTarget(entity, true, new Class[0]));
+			entity.tasks.addTask(1, new AIPassiveMobAttack(entity, 1.25D/*ConfigHandler.MOB_ATTACK_MOVEMENT_SPEED*/, true/*ConfigHandler.MOB_USE_LONG_MEMORY*/));
+			entity.targetTasks.addTask(1, new EntityAIHurtByTarget(entity, true /* ConfigHandler.MOBS_CALL_FOR_HELP*/));
 	}
 
 	static class AIPassiveMobAttack extends EntityAIAttackMelee {
-		public AIPassiveMobAttack(EntityCreature entity) {
-			// may need to adjust the speed in here (depends on the entity)
-			super(entity, 1.25D, true);
+		public AIPassiveMobAttack(EntityCreature entity, double moveSpeed, boolean longMemory) {
+			super(entity, moveSpeed, longMemory);
 		}
 
 		@Override
@@ -67,16 +51,13 @@ public class EntityAddAttackEvent {
 				attackTick = 20;
 				attacker.swingArm(EnumHand.MAIN_HAND);
 
-				// this overrides the vanilla method with out needing to fuck about with entities
-				attackEntityAsMob(attacker, entity);
+				// This overrides the vanilla method with out needing to fuck about with entities
+					attackEntityAsMob(attacker, entity); // Use our damage value attack
 			}
 		}
 
 		public boolean attackEntityAsMob(EntityCreature entityAttacker, Entity entityIn) {
-			// The damage below could be hard coded or set another way I guess maybe using an EntityDataManager object?
-			// The generic damage value attribute spams the console and says it's ignored even though it works O.o
-			boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(entityAttacker), (float) ((int) entityAttacker.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).getAttributeValue()));
-			return flag;
+			return entityIn.attackEntityFrom(DamageSource.causeMobDamage(entityAttacker), 2F/*ConfigHandler.MOB_ATTACK_DAMAGE*/);
 		}
 	}
 }
